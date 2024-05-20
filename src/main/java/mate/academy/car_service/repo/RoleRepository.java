132416b1d005d174ad2f1303bbf38d514b4c9972
Oleh <popovich158@gmail.com>
2024-05-20 23:27:27 +0300

    Formatted files which was responsible for authentication and managing authentication, user registration.
    Cars Controller: Managed car inventory (CRUD for Cars)
    Created service and implementation for using in carController.
    
    POST: /cars - add a new car
    GET: /cars - get a list of cars
    GET: /cars/ - get car's detailed information
    PUT/PATCH: /cars/ - update car (also manage inventory)
    DELETE: /cars/ - delete car

--- src/main/java/mate/academy/car_service/repo/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package mate.academy.car_service.repo;
-
-import mate.academy.car_service.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByRoleName(Role.RoleName roleName);
-}
